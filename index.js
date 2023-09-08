1fa4bbd9bb24c6725c941118d63a71ae494fadd9
Muhdsodiq Bolarinwa <105107754+Amity808@users.noreply.github.com>
2023-09-08 12:59:33 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
+let currentDate;
+let currentUTCTSelector;
+let currentDaySelectorID;
 
-
-
-// we create a function to get the current day of the week
-var currentDate = new Date()
-function getCurrentDay() {
-    const daysInWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-    const getTodayDate = currentDate.getUTCDay()
-    return daysInWeek[getTodayDate];
-}
-
-// function to get the UTC time in milliseconds
-function getCurrentUTCTime() {
-    return currentDate.getTime();
-}
-var currentUTCTSelector = document.querySelectorAll('[data-testid="currentUTCTime"]')
-var currentDaySelectorID = document.querySelectorAll('[data-testid="currentDayOfTheWeek"]')
-
-currentDaySelectorID.textContent = getCurrentDay();
-currentUTCTSelector.textContent = getCurrentUTCTime();
-
-
-var a = getCurrentDay();
-console.log(getCurrentUTCTime());
-console.log(getCurrentDay());
-console.log(new Date());
+document.addEventListener('DOMContentLoaded', function() {
+    // Create a function to get the current day of the week
+    currentDate = new Date();
+  
+    function getCurrentDay() {
+      const daysInWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+      const getTodayDate = currentDate.getUTCDay();
+      return daysInWeek[getTodayDate];
+    }
+  
+    // Function to get the UTC time in milliseconds
+    function getCurrentUTCTime() {
+      return currentDate.getTime();
+    }
+  
+    currentUTCTSelector = document.querySelector('[data-testid="currentUTCTime"]');
+    currentDaySelectorID = document.querySelector('[data-testid="currentDayOfTheWeek"]');
+  
+    currentDaySelectorID.textContent = getCurrentDay();
+    currentUTCTSelector.textContent = getCurrentUTCTime();
+  
+    var a = getCurrentDay();
+    console.log(getCurrentUTCTime());
+    console.log(getCurrentDay());
+    console.log(new Date());
+  });
+  
